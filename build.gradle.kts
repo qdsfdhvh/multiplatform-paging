280258ddfbba661a501976d49843bbeaf3b28cf5
seiko <seiko_des@outlook.com>
2022-07-22 20:56:31 +0800

    set publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("com.android.application").apply(false)
     id("com.android.library").apply(false)
     kotlin("android").apply(false)
     id("com.diffplug.spotless").version(Versions.spotless)
+    id("com.vanniktech.maven.publish").version("0.20.0").apply(false)
 }
 
 allprojects {
@@ -32,6 +36,41 @@ allprojects {
             target("**/*.java")
             targetExclude("$buildDir/**/*.java", "bin/**/*.java")
             // licenseHeaderFile(rootProject.file("spotless/license"))
+        }
+    }
+
+    group = "io.github.qdsfdhvh"
+    version = "1.0.2"
+
+    plugins.withId("com.vanniktech.maven.publish.base") {
+        @Suppress("UnstableApiUsage")
+        configure<MavenPublishBaseExtension> {
+            publishToMavenCentral(SonatypeHost.S01)
+            signAllPublications()
+            pom {
+                name.set("Multiplatform Paging")
+                description.set("Jetpack Paging3 for Kotlin Multiplatform.")
+                url.set("https://github.com/qdsfdhvh/multiplatform-paging")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Seiko")
+                        name.set("SeikoDes")
+                        email.set("seiko_des@outlook.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/qdsfdhvh/multiplatform-paging")
+                    connection.set("scm:git:git://github.com/qdsfdhvh/multiplatform-paging.git")
+                    developerConnection.set("scm:git:git://github.com/qdsfdhvh/multiplatform-paging.git")
+                }
+            }
         }
     }
 }
